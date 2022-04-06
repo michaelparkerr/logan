37977dbe51f7209dd4d7045763b1ab379110c249
k414n <50687117+michaelparkerr@users.noreply.github.com>
2022-04-06 09:34:50 +0900

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,26 @@
+git clone https://github.com/michaelparkerr/logan.git
+
+IP=$(curl ifconfig.me)
+sed -i "s|<yourip>|$IP|g" ~/logan/runnode.sh
+
+mkdir /root/ctc
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+chmod +x ~/logan/runnode.sh
+bash ~/logan/runnode.sh
